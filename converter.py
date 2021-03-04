d0a7f481bed1ef4a70e02c98af6d024a10ac741f
August Schleich <61630956+A4Atom@users.noreply.github.com>
2021-03-04 10:02:12 -0800

    GIF

---
+++ converter.py
@@ -0,0 +1,22 @@
+import imageio
+import os
+
+clip = os.path.abspath('IMG_0223.MOV')
+
+def gif_maker(input_path,target_format):
+    output_path = os.path.splitext(input_path)[0] + target_format
+
+    print(f'converting {input_path} \n to {output_path}')
+
+    reader = imageio.get_reader(input_path)
+    fps = reader.get_meta_data()['fps']
+
+    writer = imageio.get_writer(output_path,fps=fps)
+
+    for frames in reader:
+        writer.append_data(frames)
+        print(f'Frames {frames}')
+    print('Done!')
+    writer.close()
+
+gif_maker(clip,'.gif')
